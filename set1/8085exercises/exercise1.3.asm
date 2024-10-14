b8eabb85022b9091ba20b41d30972aaa9bea9d33
eleninspl <106030608+eleninspl@users.noreply.github.com>
2024-10-14 11:40:07 +0300

    Update exercise1.3.asm

--- set1/8085exercises/exercise1.3.asm
+++ set1/8085exercises/exercise1.3.asm
@@ -1,20 +1,20 @@
 START:
 	LDA 2000H	
-	MVI D,FFH		; D= -1
+	MVI D,FFH	; D= -1
 	CPI 64H		; A>99?
-	JNC CASE1 		; if yes, jump case 1
+	JNC CASE1 	; if yes, jump case 1
 
 DECA:
-	INR D 			; D= D+1
+	INR D 		; D= D+1
 	SUI 0AH		; while (A>0), do A-10
-	JNC DECA		; if (A>0) continue
+	JNC DECA	; if (A>0) continue
 	ADI 0AH		; correct negative result
-	MOV E,A 		; save units in E
-	MOV A,D 		; save tens in A
+	MOV E,A 	; save units in E
+	MOV A,D 	; save tens in A
 	RLC
 	RLC
 	RLC
-	RLC 			; tens -> MSB
+	RLC 		; tens -> MSB
 	ADD E 		; units -> LSB
 	CMA
 	STA 3000H
@@ -22,7 +22,7 @@ DECA:
 
 CASE1:		
 	CPI C8H		; A>199?
-	JNC CASE2 		; if yes, jump to case 2
+	JNC CASE2 	; if yes, jump to case 2
 	SUI 64H		; sub 100
 	JMP DECA	
 
