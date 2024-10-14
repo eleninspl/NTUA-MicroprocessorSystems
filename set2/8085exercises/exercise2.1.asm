f2faaa0120633eab528c297cc1e9c51fd4139a4b
eleninspl <106030608+eleninspl@users.noreply.github.com>
2024-10-14 11:40:55 +0300

    Update and rename exercise2.1.8085 to exercise2.1.asm

--- set2/8085exercises/exercise2.1.asm
+++ set2/8085exercises/exercise2.1.asm
@@ -4,7 +4,7 @@ MVI D,00H		; D= 00H
 LXI H,0900H		; M= HL= 0900H
 LXI B,0000H 		; BC= 0000H
 
-MOV M,A		; store 0
+MOV M,A			; store 0
 
 STORE:
 	INX H 		; HL++
@@ -12,7 +12,7 @@ STORE:
 	MOV M,A	; M= A
 	CALL RANGE
 	CALL COUNT_1	
-	CPI 7FH	; (A==127)?
+	CPI 7FH		; (A==127)?
 	JNZ STORE	; if no, store
 	JMP DONE
 
@@ -21,7 +21,7 @@ COUNT_1:
 	MVI E,08H	; E= 8
 LOOP1:
 	RLC		; rotate with carry
-	JC IS_1	; if (CY==1), BC++
+	JC IS_1		; if (CY==1), BC++
 CONT_LOOP1:
 	DCR E		; E++
 	JNZ LOOP1	; if (E!=0), loop1
@@ -33,7 +33,7 @@ IS_1:
 RANGE:
 	CPI 10H 	; (A<10H)?
 	JC NOT_RANGE	; if yes, not in range
-	CPI 61H	; (A<=60H)?
+	CPI 61H		; (A<=60H)?
 	JNC NOT_RANGE	; if no, not in range
 	INR D		; D++
 NOT_RANGE:
